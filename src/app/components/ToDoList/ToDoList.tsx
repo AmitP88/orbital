9d5a297d7e87ac3086f16a3b1eeeb81dc920d9e8
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2025-06-11 10:06:44 -0700

    added styles to ToDoList input and button

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -4,9 +4,9 @@ export default function ToDoList() {
   return (
     <div className="ToDoList">
       <h1 className="text-center">ToDoList</h1>
-      <div className="addItemInput flex flex-nowrap">
+      <div className="addItemInput">
         <input name="input-item" className="align-middle border-red-50" type="text" placeholder="type item here" required />
-        <button>Add Item</button>
+        <button className="w-full">Add Item</button>
       </div>      
       <div className="list">
 
