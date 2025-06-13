2c8a5e03ab84a6ec1e2b788c968a7463b76534c7
AmitP88 <amitpatel_7@msn.com>
2025-06-13 13:37:06 -0700

    added type=submit to button

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -4,7 +4,7 @@ export default function ToDoList() {
       <h1 className="text-center">ToDoList</h1>
       <div className="addItemInput bg-cornflower-blue h-min">
         <input name="input-item" className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" type="text" placeholder="Type item here" required />
-        <button className="mt-3 p-2 rounded-full w-full bg-teal-300 text-white">Add Item</button>
+        <button type="submit" className="mt-3 p-2 rounded-full w-full bg-teal-300 text-white">Add Item</button>
       </div>      
       <div className="list">
 
