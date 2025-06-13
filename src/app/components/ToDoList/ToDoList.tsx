b365b88b3de0de2c84fe9213037b03fdfd8a9fca
AmitP88 <amitpatel_7@msn.com>
2025-06-13 13:35:29 -0700

    improved styles of input and button

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -3,8 +3,8 @@ export default function ToDoList() {
     <div className="ToDoList">
       <h1 className="text-center">ToDoList</h1>
       <div className="addItemInput bg-cornflower-blue h-min">
-        <input name="input-item" className="align-middle border-red-50 bg-green-500" type="text" placeholder="type item here" required />
-        <button className="w-full bg-teal-300 text-red-600">Add Item</button>
+        <input name="input-item" className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" type="text" placeholder="Type item here" required />
+        <button className="mt-3 p-2 rounded-full w-full bg-teal-300 text-white">Add Item</button>
       </div>      
       <div className="list">
 
