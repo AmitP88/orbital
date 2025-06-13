37aa20439c9d11136938568d1344466191188c6d
AmitP88 <amitpatel_7@msn.com>
2025-06-13 13:24:41 -0700

    moved styles to ToDoList elements using Tailwind and deleted ToDoList.css

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -1,12 +1,10 @@
-import "./ToDoList.css"
-
 export default function ToDoList() {
   return (
     <div className="ToDoList">
       <h1 className="text-center">ToDoList</h1>
-      <div className="addItemInput">
-        <input name="input-item" className="align-middle border-red-50" type="text" placeholder="type item here" required />
-        <button className="w-full">Add Item</button>
+      <div className="addItemInput bg-cornflower-blue h-min">
+        <input name="input-item" className="align-middle border-red-50 bg-green-500" type="text" placeholder="type item here" required />
+        <button className="w-full bg-teal-300 text-red-600">Add Item</button>
       </div>      
       <div className="list">
 
