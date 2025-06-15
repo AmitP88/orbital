adeca1aab08c4c3650d7a19929810ae0c0357c81
AmitP88 <amitpatel_7@msn.com>
2025-06-14 18:43:31 -0700

    moved className attribute in input element to the end

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -3,7 +3,7 @@ export default function ToDoList() {
     <div className="ToDoList">
       <h1 className="text-center">ToDoList</h1>
       <div className="addItemInput bg-cornflower-blue h-min">
-        <input name="input-item" type="text" className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" placeholder="Type item here" />
+        <input name="input-item" type="text" placeholder="Type item here" className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" />
         <button type="submit" className="mt-3 p-2 rounded-full w-full bg-teal-300 text-white">Add Item</button>
       </div>      
       <div className="list">
