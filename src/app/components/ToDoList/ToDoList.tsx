f08649e4b1a4a3312d8e07ec666c58915950db35
AmitP88 <amitpatel_7@msn.com>
2025-06-14 19:08:45 -0700

    created handleInputChange function and called it on input element upon input change

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -1,9 +1,18 @@
-export default function ToDoList() {  
+export default function ToDoList() {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+  }
+
   return (
-    <div className="ToDoList">
+    <div className="to-do-list">
       <h1 className="text-center">ToDoList</h1>
-      <div className="addItemInput bg-cornflower-blue h-min">
-        <input name="input-item" type="text" placeholder="Type item here" className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" />
+      <div className="add-item-input bg-cornflower-blue h-min">
+        <input
+          name="input-item"
+          type="text"
+          onChange={handleInputChange}
+          placeholder="Type item here"
+          className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" />
         <button type="submit" className="mt-3 p-2 rounded-full w-full bg-teal-300 text-white">Add Item</button>
       </div>      
       <div className="list">
