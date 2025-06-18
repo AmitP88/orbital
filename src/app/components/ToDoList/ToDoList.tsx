dc4235ad0d8031332654f991d13ca31bd5fee328
AmitP88 <amitpatel_7@msn.com>
2025-06-18 02:58:33 -0700

    added new items to list upon submit

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -4,15 +4,18 @@ import { useState } from "react";
 
 export default function ToDoList() {
   const [Input, setInput] = useState('');
+  const [divs, setDivs] = useState<string[]>([]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
-    // console.log("Input Value: ", event.target.value);
   }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // prevents page reload when clicking submit button
-    console.log("Input: ", Input);
+
+    const newDivContent = Input;
+    setDivs((prevDivs) => [...prevDivs, newDivContent]);
+    
     // Optionally, clear the input after submit
     setInput('');
   }
@@ -21,17 +24,22 @@ export default function ToDoList() {
     <div className="to-do-list">
       <h1 className="text-center">ToDoList</h1>
       <div className="add-item-input bg-cornflower-blue h-min">
-      <form className="add-item-input bg-cornflower-blue h-min" onSubmit={handleSubmit}>
-        <input
-          name="input"
-          type="text"
-          value={Input}
-          onChange={handleInputChange}
-          placeholder="Type item here"
-          className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" />
-        <button type="submit" className="mt-3 p-2 rounded-full w-full bg-teal-300 text-white">Add Item</button>
-      </form>      
+        <form className="add-item-input bg-cornflower-blue h-min" onSubmit={handleSubmit}>
+          <input
+            name="input"
+            type="text"
+            value={Input}
+            onChange={handleInputChange}
+            placeholder="Type item here"
+            className="mt-3 p-1 border border-gray-300 align-middle bg-gray-200" />
+          <button type="submit" className="mt-3 p-2 rounded-full w-full bg-teal-300 text-white">Add Item</button>
+        </form>      
 
+        <div>
+          {divs.map((content, index) => (
+            <div key={index}>{content}</div>
+          ))}
+        </div>
       </div>
     </div>
   )
