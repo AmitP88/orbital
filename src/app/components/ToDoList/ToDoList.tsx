69e8411bb0b687ecaa5cb385c2dffd59f895001f
AmitP88 <amitpatel_7@msn.com>
2025-06-27 03:17:34 -0700

    added comment explaining useState<string[]>([])

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -6,6 +6,19 @@ export default function ToDoList() {
   const [Input, setInput] = useState(''); // state of the user's input data from the input element
   const [List, setList] = useState<string[]>([]); // state of the list that is created on submission of user's input data
 
+  // A breakdown of useState<string[]>([])
+
+  // useState
+  // React Hook that allows functional components to manage local state. Takes an initial value as an argument and returns an array
+  // containing the current state value and a function to update that state.
+
+  // <string[]>
+  // TypeScript type parameter that defines the type of the state variable. In this case, it specifies that the state will be an array
+  // where each element is a string.
+
+  // ([])
+  // The initial value passed to the useState Hook. It initializes the state with an empty array [].
+
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value); // creates a new Input state that contains the user's input data (event.target.value)
   }
@@ -35,7 +48,7 @@ export default function ToDoList() {
         </form>      
 
         <div className="list-none mt-12">
-          {List.map((content, index) => (
+          {List.map((content, index) => ( // "content" is the array, "index" is the key
             <div key={index} className="list-item border-2 border-gray-400 mt-3 pl-3">{content}</div>
           ))}
         </div>
