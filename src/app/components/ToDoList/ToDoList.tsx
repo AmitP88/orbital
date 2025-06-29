0fd64f826d683cafd9a05149832b150fab6fcb4a
AmitP88 <amitpatel_7@msn.com>
2025-06-29 14:28:59 -0700

    added absolute import path to 'paths' object in tsconfig for Shadcn component imports

--- src/app/components/ToDoList/ToDoList.tsx
+++ src/app/components/ToDoList/ToDoList.tsx
@@ -1,2 +1,8 @@
 // Source code: Building a Todo List App with Next.js by Asharib Ali https://asharibali.medium.com/building-a-todo-list-app-with-next-js-ed8228afff31
 
+"use client";
+import { useState, useEffect, ChangeEvent, KeyboardEvent } from "react";
+// styled components from Shadcn
+import { Input } from "@/ui/input"; 
+import { Button } from "@/ui/button";
+import { Checkbox } from "@/ui/checkbox";
