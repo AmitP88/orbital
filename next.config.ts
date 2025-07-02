cda4a94d197ff5112dee4658d7d90e9e9e23b4b9
AmitP88 <amitpatel_7@msn.com>
2025-07-02 04:18:44 -0700

    disabled UI for NextJS Toast notifications

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false // disabled UI for NextJS Toast notifications
 };
 
 export default nextConfig;
